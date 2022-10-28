928457f09e22303267d61719951c7761f414e34c
Varun Menon <53273234+VarunMenon-ux@users.noreply.github.com>
2022-10-27 22:39:05 -0700

    Added new code with testing

---
+++ src/urinalsTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class urinalsTest {
+
+    @org.junit.jupiter.api.Test
+    void testStringChar(){
+        System.out.println("====== Varun Menon Findler == TEST TWO EXECUTED =======");
+    }
+
+
+
+}
