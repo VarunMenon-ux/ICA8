278d89febe84cad7c1d32769f9dac11e25a6d25b
Varun Menon <53273234+VarunMenon-ux@users.noreply.github.com>
2022-10-28 18:41:13 -0700

    adding test one for getUrinals

--- src/urinalsTest.java
+++ src/urinalsTest.java
@@ -6,9 +6,14 @@ class urinalsTest {
 
     @org.junit.jupiter.api.Test
     void getUrinals(){
-
-
-        System.out.println("====== Varun Menon Findler == TEST TWO EXECUTED =======");
+        assertEquals(0, urinals.getUrinals("1"));
+        assertEquals(1, urinals.getUrinals("0"));
+        assertEquals(0, urinals.getUrinals("10"));
+        assertEquals(0, urinals.getUrinals("1001"));
+        assertEquals(3, urinals.getUrinals("00000"));
+        assertEquals(-1, urinals.getUrinals("110"));
+        assertEquals(-1,urinals.getUrinals("101100001"));
+        System.out.println("====== Varun Menon == TEST ONE EXECUTED =======");
     }
 
 
